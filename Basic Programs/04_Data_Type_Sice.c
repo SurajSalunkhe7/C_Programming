39f02bf8dccb7504259d4b35880afec444964c6f
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-04-07 17:48:45 +0530

    Basic

---
+++ Basic Programs/04_Data_Type_Sice.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("Storeage Size Fro int Is : %d\n",sizeof(int));
+     printf("Storeage Size Fro char Is : %d\n",sizeof(char));
+    return 0;
+}
