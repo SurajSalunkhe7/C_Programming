577be93d2ca8e7275f2db34f18aee61e9c52e94e
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-04-09 13:12:59 +0530

    Basic

---
+++ Basic Programs/08_Given_Number_Even_Number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a=0;
+
+   printf("/n Enter Number:");
+   scanf("%d",&a);
+
+   if(a%2==0)
+   {
+       printf("%d Even Number",a);
+   }
+}
+
+
