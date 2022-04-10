aded34952e1135d7b063311a8cd6856acd67487e
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-04-10 19:47:18 +0530

    Basic

---
+++ Basic Programs/09_Given_Number_Is_Odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=0;
+
+    printf("Enter A Number:\n");
+    scanf("%d",a);
+
+    if(a==!0)
+    {
+        printf("%d Given Number Is Odd",a);
+    }
+    return 0;
+}
