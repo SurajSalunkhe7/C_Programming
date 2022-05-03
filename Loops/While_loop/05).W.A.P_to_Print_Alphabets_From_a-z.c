5a0e3d193aacc39faf2524e67827d6c5d4b06f94
surajsalunkhe7 <surajsalunkhe7768@gmail.com>
2022-05-03 20:58:27 +0530

    While_Loop

---
+++ Loops/While_loop/05).W.A.P_to_Print_Alphabets_From_a-z.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int ch=0;
+    printf("Alphabets From a-z are:\n");
+
+    for(ch='a';ch<='z';ch++)
+    {
+        printf("%c\n",ch);
+    }
+    return 0;
+}
